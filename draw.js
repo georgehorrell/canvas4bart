e7212ecd140f1e5024e5326accbc576158a47d24
George Horrell <georgehorrelluk@gmail.com>
2020-04-21 22:29:48 -0700

    updated for drawing with sample rate

--- draw.js
+++ draw.js
@@ -1,16 +1,42 @@
 window.addEventListener('load', function() {
     var c = document.getElementById("myCanvas");
+    var slider = document.getElementById("myRange");
     var ctx = c.getContext("2d");
-    ctx.moveTo(0,0);
-    ctx.lineTo(200,100);
-    ctx.stroke();
 
     let points = [];
+    let sampleRate = 100;
+    let toggle = false;
+
+    function draw() {
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.beginPath();
+        let start = points[0];
+        ctx.moveTo(start.x,start.y);
+        points.slice(1).forEach(p => {
+            if (Math.random()*100 > sampleRate) {
+                return;
+            }
+            ctx.lineTo(p.x, p.y);
+            ctx.stroke();
+            ctx.moveTo(p.x, p.y);
+        });
+    }
 
     c.addEventListener('mousedown', e => {
-        console.log(e);
+        points = [{x: e.clientX, y: e.clientY}];
+    });
+    c.addEventListener('mouseup', e => {
+        draw();
     });
     c.addEventListener('mousemove', e => {
-        console.log(e);
+        if (e.buttons === 1) {
+            points.push({x: e.clientX, y: e.clientY});
+        }
     });
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+        sampleRate = this.value;
+        draw();
+    }
 });
